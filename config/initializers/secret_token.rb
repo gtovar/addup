d52d8315954fb91b64bd2b0e1fe49e103d9d4f8c
Gilberto Tovar <gtovar@incaztech.com>
2012-08-27 13:22:03 -0500

    First commit, app is create on github and heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Addup::Application.config.secret_token = '032b2e3ab8cbfbade3a2c4b191a2bd28fc2ea3429895bb17ac4ba438aeeb80b3b6d9e9e08af5a71bf77a28492107faaebf5dc298554108e5829c80c8f2faf292'
