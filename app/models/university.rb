f3b2a8ca189839770400ef48cf58950f8fb3bf2a
Gilberto Tovar <gtovar@incaztech.com>
2012-09-11 10:26:21 -0500

    Comments line of university model

--- app/models/university.rb
+++ app/models/university.rb
@@ -5,17 +5,17 @@ class University < ActiveRecord::Base
 #  attr_accessor :career_ids
 #  after_save :update_careers
 
-/*  def update_careers
-    unless career_ids.nil?
-      self.university_career_relationships.each do |ucr|
-        ucr.destroy unless career_ids.include?(ucr.career_id.to_s)
-        career_ids.delete(ucr.career_id.to_s)
-      end
-      careers_ids.each do |c|
-        self.university_career_relationships.create(:career_id => c) unless c.blank?
-      end
-      reload
-      self.career_ids = nil
-    end
-  end*/
+#  def update_careers
+#    unless career_ids.nil?
+#      self.university_career_relationships.each do |ucr|
+#        ucr.destroy unless career_ids.include?(ucr.career_id.to_s)
+#        career_ids.delete(ucr.career_id.to_s)
+#      end
+#      careers_ids.each do |c|
+#        self.university_career_relationships.create(:career_id => c) unless c.blank?
+#      end
+#      reload
+#      self.career_ids = nil
+#    end
+#  end
 end
