2b3888d508c280f737903af676cd85b8e5e25d58
Gilberto Tovar <gtovar@incaztech.com>
2012-09-25 11:48:13 -0500

    Remove order scaffold

--- app/models/order.rb
+++
@@ -1,5 +0,0 @@
-class Order < ActiveRecord::Base
-belongs_to :donation
- attr_accessor :card_number, :card_verification
- attr_accessible :card_expires_on, :card_type, :donation_id, :first_name, :ip_address, :last_name, :new
-end
