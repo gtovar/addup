9e2a32297db1c2cff990774a2c0608f471d18b75
Gilberto Tovar <gtovar@incaztech.com>
2012-09-26 14:59:55 -0500

    Test paypal

--- app/controllers/paypal_express_controller.rb
+++ app/controllers/paypal_express_controller.rb
@@ -2,4 +2,12 @@ class PaypalExpressController < ApplicationController
 include ActiveMerchant::Billing::Integrations
 def checkout
 end
+
+def notifications
+   notify = Paypal::Notification.new(request.raw_post)
+   PaymentNotification.create!(:params => params,:status => notify.status, :transaction_id => notify.transaction_id )
+render :nothing => true
+  end
+
+
 end
