3fc0371a4da2d2a9a6106bcc3dc4e3c36ffdbf69
Gilberto Tovar <gtovar@incaztech.com>
2012-11-23 16:49:10 -0600

    Delete association

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,4 @@
 class Donation < ActiveRecord::Base
-has_one :student_application
+#has_one :student_application
 attr_accessible :currency, :invoice, :mc_gross, :params, :received_at, :status, :student_application, :transaction_id
 end
