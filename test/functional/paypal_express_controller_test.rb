a929901bdf45c31739b9bee69b1bf81f30492c30
Gilberto Tovar <gtovar@incaztech.com>
2012-11-30 18:51:37 -0600

    teste

--- test/functional/paypal_express_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PaypalExpressControllerTest < ActionController::TestCase
-  test "should get checkout" do
-    get :checkout
-    assert_response :success
-  end
-
-end
--- test/unit/helpers/paypal_express_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PaypalExpressHelperTest < ActionView::TestCase
-end
