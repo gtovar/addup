0e339fe6d91906bbf17d4e2cf77876323ebc122b
Gilberto Tovar <gtovar@incaztech.com>
2012-12-13 14:38:18 -0600

    test for contant

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -48,6 +48,10 @@ class ImageUploader < CarrierWave::Uploader::Base
      process :resize_to_fill => [100,100]
    end
 
+   version :sliderhome do
+     process :resize_to_fill => [76,76]
+   end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
    def extension_white_list
