b814bc2e05dcf0d872d923ff842939579f41abca
Gilberto Tovar <gtovar@incaztech.com>
2012-12-13 09:56:15 -0600

    Session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Addup::Application.config.session_store :cookie_store, key: '_addup_session'
+Addup::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
