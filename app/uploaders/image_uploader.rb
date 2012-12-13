f518a9d8cabfde6cc374bd904e4e5edc07c84699
Gilberto Tovar <gtovar@incaztech.com>
2012-12-13 14:33:08 -0600

    TEst sendgrid 1

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,8 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files
-
-  version :sliderdetail do
+   version :sliderdetail do
      process :resize_to_fill => [580, 315]
    end
 
@@ -47,10 +46,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
    version :mosaichome do
      process :resize_to_fill => [100,100]
-   end
-
-   version :sliderhome do
-     process :resize_to_fill => [71,71]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
