d9bf269c9392232cdb4cfdc76922acc2c789a0eb
Gilberto Tovar <gtovar@incaztech.com>
2012-12-13 10:09:32 -0600

    Session table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121206173005) do
+ActiveRecord::Schema.define(:version => 20121213160656) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -63,6 +63,16 @@ ActiveRecord::Schema.define(:version => 20121206173005) do
     t.datetime "created_at",             :null => false
     t.datetime "updated_at",             :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "student_applications", :force => true do |t|
     t.string   "last_name"
